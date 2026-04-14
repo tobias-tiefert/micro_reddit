0f395a0673b878519f074dd745361f144d36102b
Tobias Tiefert <244593488+tobias-tiefert@users.noreply.github.com>
2026-04-14 17:44:22 +0200

    Add the Comment model, its migration and validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: { in: 6..25 }
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 6..25 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_14_142846) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_152512) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.integer "post_id", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
@@ -28,5 +38,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_14_142846) do
     t.string "username"
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
